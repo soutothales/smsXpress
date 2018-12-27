cb4a11fb183d6b1f790ff707c6253960230bae4e
Thales Souto <thaleshenriquepb@gmail.com>
2018-12-26 23:15:42 -0300

    implementado sistema de tags na tela de sms

--- frontend/smsXpress-frontend/src/app/app.module.ts
+++ frontend/smsXpress-frontend/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AppRoutingModule, routes } from './app-routing.module';
+// import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { SmsComponent } from './sms/sms.component';
 import { HomeComponent } from './home/home.component';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
-import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule,
+         MatChipsModule,
+         MatFormFieldModule,
+         MatChipInputEvent } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,11 +23,16 @@ import { RouterModule, Routes } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgxPageScrollModule,
-    RouterModule.forRoot(routes)
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    // AppRoutingModule,
+    // RouterModule.forRoot(routes)
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/smsXpress-frontend/src/app/sms/sms.component.ts
+++ frontend/smsXpress-frontend/src/app/sms/sms.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ENTER, SPACE } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material';
+
+
+export interface Phone {
+  phone: string;
+}
 
 @Component({
   selector: 'app-sms',
@@ -6,6 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sms.component.css']
 })
 export class SmsComponent implements OnInit {
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+
+  readonly separatorKeysCodes: number[] = [ENTER, SPACE];
+  phones: Phone[] = [ ];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our phone
+    if ((value || '').trim()) {
+      this.phones.push({phone: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+
+    console.log(this.phones);
+  }
+
+  remove(phone: Phone): void {
+    const index = this.phones.indexOf(phone);
+
+    if (index >= 0) {
+      this.phones.splice(index, 1);
+    }
+  }
 
   constructor() { }
 
